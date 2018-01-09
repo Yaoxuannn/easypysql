37d140e36a8b0d71d0f15b35dc294cb4c6625748
Justin13wyx <justin13wyx@gmail.com>
2018-01-10 00:36:22 +0800

    Add support for mysql (beta)

--- easypysql/connector/mysql_connector.py
+++ easypysql/connector/mysql_connector.py
@@ -5,19 +5,54 @@ from .base import *
 
 
 class MysqlConnector(BaseConnector):
-    def __init__(self):
+    def __init__(self, username=None, password=None, host=None, port=None, database=None, timeout=10,
+                 autocommit=False, **kwargs):
         super(MysqlConnector, self).__init__()
-
-
+        self._db = database
+        self.target = "MySQL"
+        self.target_info = ""
+        self.attribute = {
+            "host": host,
+            "port": port,
+            "user": username,
+            "password": password,
+            "database": database,
+            "connect_timeout": timeout
+        }
+        self.attribute.update(kwargs)
+        self.connect()
+        if autocommit:
+            self._conn.autocommit(1)
 
     def connect(self):
-        pass
-
+        """
+        Establish the connection to the mysql database.
+        """
+        try:
+            self._conn = pymysql.connect(**self.attribute)
+        except pymysql.err.OperationalError as e:
+            raise ConnectionException(e)
+        if self._conn:
+            self.cursor = self._conn.cursor()
+            self.target_info = "MySQL %s" % self._conn.server_version
 
     @property
     def transaction(self):
-        pass
+        """
+        Seems pymysql doesn't provides with methods which can
+        return the status of transaction of mysql server.
+
+        This is a ugly implement version of querying transaction status
+        """
+        sql = "SELECT trx_id FROM information_schema.INNODB_TRX"
+        self.cursor.execute(sql)
+        if self.cursor.fetchall():
+            return True
+        return False
 
     @property
     def database(self):
-        pass
+        return self.database
+
+    def select_db(self, database):
+        self._conn.select_db(database)
