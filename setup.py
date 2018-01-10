045055429cbd050a142e404440cba3a2646a4dca
Justin13wyx <justin13wyx@gmail.com>
2018-01-10 21:40:31 +0800

    Exception occured, Fix it

--- setup.py
+++ setup.py
@@ -4,12 +4,11 @@ from setuptools import setup
 
 setup(
     name='easypysql',
-    version='0.1.1',
+    version='0.1.2',
     author='Justin13',
     author_email='justin13wyx@gmail.com',
     url='https://github.com/Justin13wyx/easypysql',
     description='A simple and small python ORM framework',
-    # long_description=open("README.md", encoding="utf-8").read(),
     packages=['easypysql'],
     install_requires=["pymysql"],
 
